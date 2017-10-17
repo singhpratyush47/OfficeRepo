4c855286983d5d8c7846330a0a7d68cf8e1c6ad1
singhpratyush47 <singhpratyush47@gmail.com>
2017-10-17 18:48:21 +0530

    second commits

--- test/src/com/test/test.java
+++ test/src/com/test/test.java
@@ -1,7 +1,9 @@
 package com.test;
 
 public class test {
-public static void main(String[] args) {
-	System.out.println("hello");
-}
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
 }
